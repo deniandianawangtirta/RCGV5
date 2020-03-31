be9d8d989e6a6ef693147e6309fda007bd049999
raincityhack <42696972+raincityhack@users.noreply.github.com>
2020-03-31 21:55:19 +0700

    Add files via upload

---
+++ start-setup.sh
@@ -0,0 +1,5 @@
+if [ -d "/storage/emulated/0/RCGV5" ] ; then
+rm -rf /storage/emulated/0/RCGV5
+fi
+cp -avr /data/data/com.termux/files/home/RCGV5 /storage/emulated/0/
+rm /data/data/com.termux/files/home/RCGV5 
