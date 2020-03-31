6cd55b71f78fc185b7d1026316daaf7afabfa240
raincityhack <42696972+raincityhack@users.noreply.github.com>
2020-03-31 22:01:54 +0700

    Delete start-setup.sh

--- start-setup.sh
+++
@@ -1,5 +0,0 @@
-if [ -d "/storage/emulated/0/RCGV5" ] ; then
-rm -rf /storage/emulated/0/RCGV5
-fi
-cp -avr /data/data/com.termux/files/home/RCGV5 /storage/emulated/0/
-rm /data/data/com.termux/files/home/RCGV5 
